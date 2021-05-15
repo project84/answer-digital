30374d2b103d2ee8bbfadc76268918b492686a62
Philip Rozycki-Burn <pr@neoftl.com>
2021-05-15 22:46:37 +0100

    Added function to validate post submission form

--- cypress/pages/practice-form.js
+++ cypress/pages/practice-form.js
@@ -1,5 +1,10 @@
+import dayjs from 'dayjs';
+
+import * as table from '../pages/components/table';
+
 import defaultValues from '../fixtures/practice-form/default-values.json';
 import fieldTypes from '../fixtures/practice-form/field-types.json';
+import submissionMapping from '../fixtures/practice-form/submission-mapping.json';
 
 /* Page element selectors */
 /* Basic page attributes */
@@ -16,18 +21,18 @@ export const form = {
 	mobile: '#userNumber',
 	dob: '#dateOfBirthInput',
 	subjects: '#subjectsContainer',
+	subjectsDropDown: '.subjects-auto-complete__menu',
 	hobbies: '[type="checkbox"]',
 	picture: '#uploadPicture',
 	address: '#currentAddress',
 	state: '#state',
-	city: '#city',
-	submit: '#submit'
+	city: '#city'
 }
 
+export const submitButton = '#submit';
+
 /* Success modal */
-export const modal = {
-	content: '.modal-content'
-}
+export const modal = '.modal-content';
 
 /**
  * Visit the practice form page and verify the page title
@@ -93,11 +98,14 @@ export function selectGender(gender) {
 export function selectSubjects(subjects = []) {
 
 	subjects.forEach(subject => {
-		// Type each subject into the form field, tabbing between each entry
+		// Type each subject into the form field
 		cy.get(form.subjects)
-			.type(subject)
-			.focused()
-			.tab();
+			.type(subject);
+
+		// Verify that a single item is displayed in the drop-down and select it
+		cy.get(form.subjectsDropDown)
+			.should('have.length', 1)
+			.click();
 	});
 
 }
@@ -150,5 +158,104 @@ export function selectLocation(state, city) {
 				.type(city + '{enter}');
 		}
 	}
+
+}
+
+/**
+ * Verify that the post submission form has the expected values
+ * @param {object} expectedValues 
+ */
+export function validateSubmission(expectedValues) {
+
+	let populatedFields = [];
+	let missingFields = []
+
+	submissionMapping.direct.forEach(mapping => {
+
+		if (expectedValues[mapping.selector]) {
+			populatedFields.push({
+				label: mapping.label,
+				expValue: expectedValues[mapping.selector]
+			});
+		} else {
+			missingFields.push(mapping.label);
+		}
+
+	});
+
+	// Add form fields with concatenated values
+	submissionMapping.concat.forEach(mapping => {
+
+		let expValue = '';
+
+		mapping.selectors.forEach(selector => {
+
+			// Generate string concatenating all expected values for the field with the specified delimeter
+			const rawvalue = expectedValues[selector];
+			const value = Array.isArray(rawvalue) ? expectedValues[selector].join(mapping.delimeter) : rawvalue;
+			
+			if (value) {
+				expValue += expValue.length === 0 ? value : mapping.delimeter + value;
+			}
+
+		});
+
+		if (expValue) {
+			populatedFields.push({
+				label: mapping.label,
+				expValue: expValue
+			});
+		} else {
+			missingFields.push(mapping.label);
+		}
+
+	});
+
+	submissionMapping.list.forEach(mapping => {
+
+		let values = expectedValues[mapping.selector];
+
+		if (values) {
+			values.forEach(value => {
+				populatedFields.push({
+					label: mapping.label,
+					expValue: value
+				});
+			});			
+		} else {
+			missingFields.push(mapping.label);
+		}
+		
+	});
+
+	// Add picture name if it exists
+	if (expectedValues.picture) {
+		populatedFields.push({
+			label: submissionMapping.picture,
+			expValue: expectedValues.picture.slice(expectedValues.picture.lastIndexOf('/') + 1)
+		});
+	} else {
+		missingFields.push(submissionMapping.picture);
+	}
+
+	// Add DOB if it exists, or todays date (reflects default value in form)
+	populatedFields.push({
+		label: submissionMapping.dob,
+		expValue: dayjs(expectedValues.dob).format('DD MMMM,YYYY')
+	});
+
+	// Verify each cell in the table with a value contains the expected value
+	populatedFields.forEach(mapping => {
+		table.getCell('Label', mapping.label, 'Values').then(cell => {
+			expect(cell).to.contain(mapping.expValue);
+		});
+	});
+
+	// Verify each cell without a value is empty
+	missingFields.forEach(field => {
+		table.getCell('Label', field, 'Values').then(cell => {
+			expect(cell).to.be.empty;
+		});
+	});
 
 }
