b66d3c7063247f54e8ca51da71dd08f3900b8eaa
Philip Rozycki-Burn <pr@neoftl.com>
2021-05-15 19:40:22 +0100

    Added date picker component and command for selecting date

---
+++ cypress/pages/components/date-picker.js
@@ -0,0 +1,5 @@
+export const yearSelect = '.react-datepicker__year-select';
+export const monthSelect = '.react-datepicker__month-select';
+export const calendar = '.react-datepicker__month';
+export const day = '.react-datepicker__day--';
+export const outsideMonth ='.react-datepicker__day--outside-month';
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,52 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import dayjs from 'dayjs';
+import * as datePicker from '../pages/components/date-picker';
+
+/** 
+ * Select a date using the subject date picker
+ * @param {string} targetDate
+ */
+Cypress.Commands.add(
+	'selectDate',
+	{
+		prevSubject: true
+	},
+	(subject, targetDate) => {
+
+		// Open date picker and store current date
+		cy.wrap(subject)
+			.click()
+			.invoke('val').then(date => {
+				cy.wrap(dayjs(date), { log: false }).as('currentDate');
+			});
+
+		cy.wrap(dayjs(targetDate), { log: false }).then(targetDate => {
+			cy.get('@currentDate').then(currentDate => {
+
+				// Navigate to correct year if required
+				if (targetDate.year() != currentDate.year()) {
+					cy.get(datePicker.yearSelect)
+						.select(targetDate.format('YYYY'));
+				}
+
+				// Navigate to correct month if required
+				if (targetDate.month() != currentDate.month()) {
+					cy.get(datePicker.monthSelect)
+						.select(targetDate.format('MMMM'));
+				}
+
+				// Select date from calendar
+				cy.get(datePicker.calendar)
+					.children()
+					.find(datePicker.day + targetDate.date().toString().padStart(3, 0))
+					.not(datePicker.outsideMonth)
+					.click();
+
+			});
+		});
+
+	}
+)
