25111e20ea4f9230b766e2d869d31003a075855a
Philip Rozycki-Burn <pr@neoftl.com>
2021-05-16 07:32:16 +0100

    Created alerts module and test

---
+++ cypress/pages/alerts.js
@@ -0,0 +1,10 @@
+/* Page element selectors */
+/* Basic page attributes */
+export const url = 'alerts';
+export const pageTitle = 'Alerts';
+
+/* Alert control selectors */
+export const alertButton = '#alertButton';
+export const delayAlertButton = '#timerAlertButton';
+export const confirmButton = '#confirmButton';
+export const promptButton = '#promtButton';
