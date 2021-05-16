043a0b3b20afec8bc716e23383379627548d0b48
Philip Rozycki-Burn <pr@neoftl.com>
2021-05-16 08:47:08 +0100

    Added close button to modal component

--- cypress/pages/components/modals.js
+++ cypress/pages/components/modals.js
@@ -4,3 +4,4 @@ export const header = '.modal-header';
 export const title = '.modal-title';
 export const body = '.modal-body';
 export const footer = '.modal-footer';
+export const close = '.close';
