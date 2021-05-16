15e40f13a659714ef985793925ed0f82065ae72f
Philip Rozycki-Burn <pr@neoftl.com>
2021-05-16 07:31:53 +0100

    Refactored page visit as cypress command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('visitPage', (url, pageTitle) => {
+
+	cy.visit(url);
+	cy.get('.main-header')
+		.should('contain', pageTitle);
+
+});
