568943bb8803f88b72b7599d23ae2ff0fe8eb691
Philip Rozycki-Burn <pr@neoftl.com>
2021-05-16 07:57:58 +0100

    Created tool tips module

---
+++ cypress/pages/tool-tips.js
@@ -0,0 +1,9 @@
+/* Page element selectors */
+/* Basic page attributes */
+export const url = 'tool-tips';
+export const pageTitle = 'Tool Tips';
+
+/* Tool tip control selectors */
+export const button = '#toolTipButton';
+export const textField = '#toolTipTextField';
+export const hoverText = '.tooltip-inner';
